3a83aa9a1149fcc7ad1637ddadaa40a22aab9bcc
Azalea (on HyDEV-Daisy) <me@hydev.org>
2022-04-03 02:45:27 -0400

    [+] Configurable time_step

--- sgs/calculations.py
+++ sgs/calculations.py
@@ -7,6 +7,8 @@ import numpy
 import numpy as np
 import parselmouth
 
+from sgs.config import sgs_config
+
 
 def calculate_tilt(sound: parselmouth.Sound) -> float | None:
     """
@@ -79,8 +81,8 @@ def calculate_freq_info(audio: parselmouth.Sound, show_plot=False) -> numpy.ndar
     :param audio: Sound input
     :return: 2D Array (Each row is 1/100 of a second, row[0] is pitch (fundamental frequency), row[1:4] is formant)
     """
-    pitch_values = audio.to_pitch(0.01).selected_array['frequency']
-    formant_values = audio.to_formant_burg(0.01)
+    pitch_values = audio.to_pitch(sgs_config.time_step).selected_array['frequency']
+    formant_values = audio.to_formant_burg(sgs_config.time_step)
     result = numpy.ndarray([len(pitch_values), 4], 'float32')
 
     for i in range(len(pitch_values)):
---
+++ sgs/config.py
@@ -0,0 +1,5 @@
+class Config:
+    time_step: float = 0.01
+
+
+sgs_config = Config()
