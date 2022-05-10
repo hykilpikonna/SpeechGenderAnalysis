6d324f55e65209d22f049b57e15a8951d9385a90
Hykilpikonna <me@hydev.org>
2022-05-10 00:00:01 -0400

    [O] Limit GPU usage

--- experiment/ina_main.py
+++ experiment/ina_main.py
@@ -16,7 +16,12 @@ from PIL import Image
 from inaSpeechSegmenter import *
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
+import tensorflow as tf
 
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
 
 seg = Segmenter()
 
